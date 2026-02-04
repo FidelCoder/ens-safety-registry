35aa1f0ae252d3debe3c994f1beda65e002eef80
FidelCoder <griffinesonyango@gmail.com>
2026-02-04 15:21:07 +0300

    fix: simplify deploy script to accept private key from command line

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -6,9 +6,7 @@ import "../src/SafetyRegistry.sol";
 
 contract DeployScript is Script {
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        
-        vm.startBroadcast(deployerPrivateKey);
+        vm.startBroadcast();
         
         SafetyRegistry registry = new SafetyRegistry();
         
